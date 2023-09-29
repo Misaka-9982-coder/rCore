1852d460f56830baa782184d3dd1464c9a6b9590
Misaka <chenborun1101@gmail.com>
2023-09-29 13:23:39 +0800

    ch1 3.2

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,8 +1,42 @@
 #![no_std]
 #![no_main]
 
+
+use core::fmt::{self, Write};
 mod lang_items;
 
+const SYSCALL_WRITE: usize = 64;
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+  syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
 
 const SYSCALL_EXIT: usize = 93;
 
@@ -27,6 +61,6 @@ pub fn sys_exit(xstate: i32) -> isize {
 
 #[no_mangle]
 extern "C" fn _start() {
+    println!("Hello, world!");
     sys_exit(9);
 }
-
