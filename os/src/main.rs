936c0d59da8f8cc9de010b70a304995d8cf4c7f7
Misaka <chenborun1101@gmail.com>
2023-09-29 14:54:53 +0800

    ch1 4.2

--- os/src/main.rs
+++ os/src/main.rs
@@ -5,7 +5,9 @@ mod lang_items;
 mod sbi;
 use crate::sbi::shutdown;
 
+core::arch::global_asm!(include_str!("entry.asm"));
+
 #[no_mangle]
-extern "C" fn _start() {
+pub fn rust_main() -> ! {
     shutdown();
 }
